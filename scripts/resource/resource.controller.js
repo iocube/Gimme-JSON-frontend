5e79dd44ea182a5f0d304e98e3b4ecf7066e2993
Vladimir <edoomik@gmail.com>
2016-05-06 16:45:00 +0300

    added side panel ui

--- scripts/resource/resource.controller.js
+++ scripts/resource/resource.controller.js
@@ -7,6 +7,8 @@
   function ResourceController(resourceAPIService) {
       var self = this;
 
+      self.isPanelOpen = false;
+
       resourceAPIService.get().then(function(resources) {
         self.resources = resources;
       })
@@ -21,6 +23,7 @@
 
         resourceAPIService.post(newResource).then(function(resource) {
           self.resources.push(resource);
+          self.openPanel();
         });
       }
 
@@ -35,6 +38,22 @@
         resourceAPIService.put(resource._id.$oid, resource);
       }
 
-      self.edit = function(resource) {}
+      self.edit = function(resource) {
+      }
+
+      self.openPanel = function() {
+          self.isPanelOpen = true;
+      }
+
+      self.closePanel = function() {
+        self.isPanelOpen = false;
+      }
+
+      self.resourceEdit = {
+        'endpoint': '/api/v1/',
+        'methods': ['GET'],
+        'response': '{}',
+        'queryParams': []
+      }
   }
 })();
