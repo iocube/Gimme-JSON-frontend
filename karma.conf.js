a0739c7971d09be7d2becda36ace3c17ba51f376
Vladimir <edoomik@gmail.com>
2016-05-09 11:28:27 +0300

    added karma-coverage plugin for code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -39,7 +39,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'scripts/**/*.html': ['ng-html2js']
+      'scripts/**/*.html': ['ng-html2js'],
+      'scripts/**/*.js': ['coverage']
     },
 
     ngHtml2JsPreprocessor: {
@@ -48,7 +49,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
